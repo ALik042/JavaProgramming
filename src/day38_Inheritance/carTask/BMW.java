39378c45171f44421d4135daa1f9812a96e65fc3
ALik042 <ali.f.ismayilov@gmail.com>
2022-01-13 10:22:48 -0500

    day38: Inheritance - -Method overriding

---
+++ src/day38_Inheritance/carTask/BMW.java
@@ -0,0 +1,11 @@
+package day38_Inheritance.carTask;
+
+import day38_Inheritance.carTask.Car;
+
+public class BMW extends Car {
+    public BMW(String brand, String model, int year, double price, String color, int miles) {
+        super(brand, model, year, price, color, miles);
+    }
+
+
+}
---
+++ src/day38_Inheritance/carTask/Toyota.java
@@ -0,0 +1,11 @@
+package day38_Inheritance.carTask;
+
+import day38_Inheritance.carTask.Car;
+
+public class Toyota extends Car {
+    public Toyota(String brand, String model, int year, double price, String color, int miles) {
+        super(brand, model, year, price, color, miles);
+    }
+
+
+}
